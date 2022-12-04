91aed49612c6498e432f50365950bf5dc64ada6c
ESMatyushkin <e.s.matushkin001@yandex.ru>
2022-12-04 15:41:18 +0300

    upd(001): First template iter

--- sampleproject/src/App.vue
+++ sampleproject/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="block_logo">
+      <img id="logo" alt="Vue logo" src="./assets/MainLogo.png" />
+      <p id="title">ESM Sample</p>
+    </div>
+    <Container msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from "./components/Container.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Container,
+  },
+};
 </script>
 
 <style lang="scss">
@@ -24,5 +27,45 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  box-sizing: border-box;
+}
+
+#logo {
+  width: 128px;
+}
+
+#title {
+  font-size: 24px;
+  color: #46505bb1;
+  font-family: cursive;
+}
+
+.block_logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 64px;
+}
+
+@media (max-width: 640px) {
+  #logo {
+    width: 64px;
+  }
+
+  #title {
+    font-size: 16px;
+    color: #46505bb1;
+    font-family: cursive;
+  }
+
+  .block_logo {
+    justify-content: left;
+    align-items: flex-end;
+    margin-bottom: 24px;
+  }
+
+  #app {
+    margin-top: 24px;
+  }
 }
 </style>
